28a0f4a5f278cb16072301f9a2429f9927e7b669
cayasso <cayasso@gmail.com>
2016-11-03 14:04:35 -0600

    allow passing value to progress indicator

--- progress.js
+++ progress.js
@@ -1,22 +1,26 @@
 window.progress = (function component(options) {
+  return function create(val) {
 
-  function toggle(e) {
-    const el = document.getElementById('progress')
-    isHidden(el) ? show(el) : hide(el)
-    e.preventDefault()
+    function toggle(e) {
+      const el = document.getElementById('progress')
+      isHidden(el) ? show(el) : hide(el)
+      e.preventDefault()
+    }
+
+    function isHidden(el) {
+      return !!/hide/.test(el.className)
+    }
+
+    function hide(el) {
+      if (!isHidden(el)) el.className += ' hide'
+    }
+
+    function show(el) {
+      el.querySelector('.progress-status').style.width = val + '%'
+      el.querySelector('.indicator-value').innerHTML = '$' + val
+      el.className = el.className.replace('hide', ' ')
+    }
+
+    return { toggle }
   }
-
-  function isHidden(el) {
-    return !!/hide/.test(el.className)
-  }
-
-  function hide(el) {
-    if (!isHidden(el)) el.className += ' hide'
-  }
-
-  function show(el) {
-    el.className = el.className.replace('hide', ' ')
-  }
-
-  return { toggle }
 }())
